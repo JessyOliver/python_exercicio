25c6dd3bda62f349dc49e2bfa072b22c3f68f37c
jfran192 <jfran192@ford.com>
2022-02-09 15:28:42 -0300

    feat(ex008): Write a program that reads a value in meters and the display converted to centimeters and millimeters

---
+++ ex008.py
@@ -0,0 +1,7 @@
+# Escreva um programa que leia um valor em metros e o
+# exiba convertido em centimetros e milimetros
+
+metros = float(input("Informe o valor em metros: \n"))
+
+print("centimetros:  ", metros / 0.010000)
+print("milimetros:  ", metros / 0.0010000)
