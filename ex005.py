1c19e0fe1f50682d3ddf89fea738d6711bfa4428
jfran192 <jfran192@ford.com>
2022-02-09 15:25:44 -0300

    feat(ex005): predecessor and successor number

---
+++ ex005.py
@@ -0,0 +1,7 @@
+# Faça um programa que leia um número Inteiro e mostre
+# na tela o seu sucessor e seu antecessor.
+num = int(input("Digite um número inteiro: \n "))
+
+print('\n O numero', num - 1,
+      'é o antecessor do numero {}'.format(num),
+      'e seu sucessor é o número', num + 1)
