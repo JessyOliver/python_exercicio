25ef770815b912ef2ccb6ec1d0abe81fc3fe9c57
jfran192 <jfran192@ford.com>
2022-02-09 15:30:43 -0300

    feat(ex0010): Write a program that reads the width and height of a wall in meters, calculates its area and the amount of paint needed to paint it. Knowing that each liter of paint paints an area of 2m²

---
+++ ex011.py
@@ -0,0 +1,11 @@
+# Faça um programa que leia a largura e a altura
+# de uma parede em metros, calcule a sua area e a quantidade
+# de tinta necessária para pintá-la. S abendo que cada litro de tinta
+# pinta uma area de 2m²
+
+altura = float(input("Digite a altura da parede: "))
+base = float(input("Digite a base da parede: "))
+
+area = altura * base
+
+
