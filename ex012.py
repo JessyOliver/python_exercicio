3a449a44d1f4123c09f697f74bbfd6312a9f25da
jfran192 <jfran192@ford.com>
2022-02-15 13:56:13 -0300

    feat(ex012):make an algorithm that reads the price of a product and displays its new price, with a 5% discount

---
+++ ex012.py
@@ -0,0 +1,15 @@
+# faça um algoritmo que leia o preço de um
+# produto e mostre seu novo preço, com 5% de desconto
+
+nameProduct = str(input("Digite o nome do produto:"))
+
+price = float(input("Digite o preço do produto: "))
+
+pay = float(0)
+
+pay = price - ((5 / 100) * price)
+
+print("\n Produto:", nameProduct,
+      "\n Valor produto: R$ {:.2f}".format(price),
+      "\n Valor com desconto: R$ {:.2f}".format(pay))
+
