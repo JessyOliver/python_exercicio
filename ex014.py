8f72d7b6de80d8aab3eae231c8154645f88186fc
jfran192 <jfran192@ford.com>
2022-03-02 12:28:34 -0300

    feat(ex014):Convert temperature °C for °F.

---
+++ ex014.py
@@ -0,0 +1,5 @@
+# converter temperatura de °C para °F
+
+temperature = float(input('Digite a temperatura em °C: '))
+
+print('A temperatura em {}°C corresponde a {}°F.'.format(temperature, ((9 * temperature) / 5) + 32))
